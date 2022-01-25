863565094e99ac1b5766eea9c5f2fb1f6489de38
Lenovo <somusp75@gmail.com>
2022-01-25 22:38:02 +0530

    UC-1 ValidateFirst Name

--- Regex_User-Registration/Program.cs
+++ Regex_User-Registration/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace Regex_User_Registration
 {
-    class Program
+    class UserRegister:Validator
     {
+        public void ValidateFirstName(string firstName)
+        {
+            var expr = @"^[A-Z]{1}[a-z]{2,}$";
+            var match = Regex.Match(firstName, expr, RegexOptions.None);
+            if (match.Success)
+                Console.WriteLine("Valid First Name");
+            else
+                Console.WriteLine("Invalid First Name");
+
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            UserRegister user = new UserRegister();
+
+            string firstName = Input.inputName(); // taking first name from user
+            user.ValidateFirstName(firstName); // validating first name
         }
     }
 }
